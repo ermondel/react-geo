1a03a5b04ddf4d1ef8e21cd0e1ec086ffe3e896a
ermondel <ermondel@gmail.com>
2021-03-15 22:43:41 +0200

    feat: change the structure of the modal window

--- src/modal/ModalWindow.js
+++ src/modal/ModalWindow.js
@@ -6,36 +6,16 @@ export const modalOpen = () => ({ type: MODAL_OPEN });
 
 export const modalClose = () => ({ type: MODAL_CLOSE });
 
-export const ModalWindow = ({ visible, onWindowClose, children, styleType }) => {
-  const stopPropagation = (event) => event.stopPropagation();
-
-  const button = (
-    <button
-      className='modal__btn-close'
-      onClick={onWindowClose}
-      title='Close window'
-    >
-      <span>Close window</span>
-    </button>
-  );
-
-  let modalClassName = 'modal';
-  if (styleType) modalClassName += ` modal--${styleType}`;
-  if (!visible) modalClassName += ` modal--hide`;
-
-  const content = (
-    <div className={modalClassName}>
+export const ModalWindow = ({ visible, children, onClose }) => {
+  return ReactDOM.createPortal(
+    <div className={`modal ${visible ? '' : ' modal--hide'}`}>
       <div className='modal__background'></div>
-      <div className='modal__container' onClick={onWindowClose}>
-        <div className='modal__content' onClick={stopPropagation}>
-          <div className='modal__btns'>
-            <div className='modal__close'>{button}</div>
-          </div>
+      <div className='modal__container' onClick={onClose}>
+        <div className='modal__content' onClick={(e) => e.stopPropagation()}>
           <div className='modal__body'>{children}</div>
         </div>
       </div>
-    </div>
+    </div>,
+    document.getElementById('modal')
   );
-
-  return ReactDOM.createPortal(content, document.getElementById('modal'));
 };
